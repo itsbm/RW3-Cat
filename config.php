1c8bc2abe362773e01f9230a4e3ec88ead5dba9b
itsbm <sbm999@gmail.com>
2017-01-04 13:54:39 -0800

    config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+// ENTER YOUR CONFIG INFORMATION HERE
+//----------------------------------------
+
+$dbhost="localhost";
+$dbuser="dbuser";
+$dbpass="dbpass";
+$dbname="dbname";
+
+//-------------------------------------------
+
+?>
