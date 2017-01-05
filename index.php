8757399a3ccb5fca0b12323e915cb61d592ae29a
itsbm <sbm999@gmail.com>
2017-01-04 20:04:02 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <html>
-<title><?php print 'Library Books'; ?></title>
+<head>
+<link rel="stylesheet" href="main.css" type="text/css">
+</head>
+
 <?php
 //PHP7 script to allow public user interface with readerware mysql database
+//2017.01.03
 
+require 'config.php';
 $now = time();
 
-echo "<p><h2>Library Books</h2></p>";
-echo "This is a list of our books!<br>";
-print 'Page loaded on ';
-print date("l, F j Y \a\\t h:i a", $now);
+print "<title> $sitetitle </title>";
+print  "<p><h2><center>$sitename</center></h2></p>";
+print "<center>$sitedesc</center><br>";
+print "<center>Page loaded on ";
+print date("l, F j Y \a\\t h:i a</center>", $now);
 
 echo "<br><hr>";
 
 // Create connection
-$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbbook);
-
+$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -29,27 +34,33 @@ elseif ($_GET['sort'] == 'author')
 { $sql .= " ORDER BY SORT_NAME";
 }
 
+//$sql = "SELECT TITLE, AUTHOR, PAGES FROM READERWARE";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    echo "<center><table width='90%' border='1'> 
-	<tr>
-	<th align='left'><a href='index.php?sort=title'>TITLE</a></th>
-	<th align='left'><a href='index.php?sort=author'>AUTHOR</a></th>
-	<th align='left'>DESCRIPTION</th>
-	<th>RELEASED</th>
-	<th>PAGES</th>
-	</tr>";
+    echo "<center><table>
+        <tr class='header'>
+        <th align='left'><a href='index.php?sort=title'>TITLE</a></th>
+        <th align='left'><a href='index.php?sort=author'>AUTHOR</a></th>
+        <th align='left'>DESCRIPTION</th>
+        <th>RELEASED</th>
+        <th>PAGES</th>
+        </tr>";
     // output data of each row
 
+//echo '<img src="data:image/jpeg;base64,'.base64_encode($row['IMAGE1_DATA']).'"/>'
+
     while($row = $result->fetch_assoc()) {
+        //$rownum ++;
+        //$altrow = ($rownum % 2 == 0)? 'white': '#ddd';
+        //echo "<tr bgcolor='$altrow'>
         echo "<tr>
-	<td width='30%'><b>".$row["TITLE"]."</b></td>
-	<td width='20%'><i>".$row["SORT_NAME"]."</i></td>
-	<td>".$row["PRODUCT_INFO"]."</td>
-	<td>".$row["RELEASE_DATE"]."</td>
-	<td>".$row["PAGES"]."</td>
-	</tr>" ;
+        <td width='30%'><b>".$row["TITLE"]."</b></td>
+        <td width='20%'><i>".$row["SORT_NAME"]."</i></td>
+        <td>".$row["PRODUCT_INFO"]."</td>
+        <td>".$row["RELEASE_DATE"]."</td>
+        <td>".$row["PAGES"]."</td>
+        </tr>" ;
     }
     echo "</table></center>";
 } else {
